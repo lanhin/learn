4c2adab7a8584af03eb471e82e92537d245a08b5
lanhin <lanhin1@gmail.com>
2017-08-17 11:05:58 +0800

    Removed _LoggerHook

--- tensorflow/cifar10/cifar10_dist_train.py
+++ tensorflow/cifar10/cifar10_dist_train.py
@@ -250,47 +250,6 @@ def train():
     
     # global variables region end
 
-    '''
-    class _LoggerHook(tf.train.SessionRunHook):
-      """Logs loss and runtime."""
-
-      def begin(self):
-        print ("is this a init?")
-        self._step = -1
-        self._start_time = time.time()
-
-      def before_run(self, run_context):
-        self._step += 1
-        return tf.train.SessionRunArgs(loss)  # Asks for loss value.
-
-      def after_run(self, run_context, run_values):
-        if self._step % FLAGS.log_frequency == 0:
-          current_time = time.time()
-          duration = current_time - self._start_time
-          self._start_time = current_time
-
-          loss_value = run_values.results
-          examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
-          sec_per_batch = float(duration / FLAGS.log_frequency)
-
-          format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                        'sec/batch)')
-          print (format_str % (datetime.now(), self._step, loss_value,
-                               examples_per_sec, sec_per_batch))
-    '''
-
-    #with tf.train.MonitoredTrainingSession(
-        # added by lanhin
-    #    master=server.target,
-    #    is_chief=(FLAGS.task_index==0),
-        # added by lanhin end
-    #    checkpoint_dir=FLAGS.train_dir,
-    #    hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
-               #tf.train.NanTensorHook(loss),
-    #           _LoggerHook()],
-    #    config=tf.ConfigProto(
-    #        log_device_placement=FLAGS.log_device_placement)) as mon_sess:
-
     #lanhin start
     sv = tf.train.Supervisor(
       is_chief=True,#is_chief,
